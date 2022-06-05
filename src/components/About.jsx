9b9cd86c406b10041394b4103e265aded8320f4f
Ivan Panussis <ivan.panussis.c@gmail.com>
2022-06-05 03:49:41 -0400

    About section initial design ready

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import Img from '../assets/perfil.jpg';
 
 export const About = () => {
     return (
-        <section id="about" className='about__section'>
-            <h1>About</h1>
+        <section id="about" className='about__section border'>
+            <div className='about__title border'>
+                <h5>Get to Know</h5>
+                <h1>About Me</h1>
+            </div>
+
+            <div className='about__container border'>
+                <div className='about__image border'>
+                    <div className='image__container'>
+                        <img src={ Img } alt="profile_photo"/>
+                    </div>
+                </div>
+                <div className='about__description border'>
+                    <div className='about__achievements border'>
+                        <div>
+                            <i className="fa-solid fa-award"></i>
+                            <p>Experience</p>
+                            <small>+3 Years</small>
+                        </div>
+                        <div>
+                            <i className="fa-solid fa-medal"></i>
+                            <p>Courses</p>
+                            <small>+7 Courses</small>
+                        </div>
+                        <div>
+                            <i className="fa-solid fa-suitcase"></i>
+                            <p>Projects</p>
+                            <small>+3 Projects</small>
+                        </div>
+                    </div>
+
+                    <div className='about__me border'>
+                        <p> 
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam veritatis praesentium amet aut. Optio unde tempore ut incidunt suscipit non perferendis quisquam, nihil minus officia odit necessitatibus dolor itaque quaerat. 
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque distinctio natus architecto dolorum voluptas nemo dicta rerum sed. Adipisci officia atque dolorum nihil quidem ab ad non minima est vel.
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempore iusto error suscipit repellat inventore impedit illum ullam vero saepe reprehenderit obcaecati quaerat porro, harum esse vitae dolorum temporibus perspiciatis.
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, numquam tempore. Aut nobis laborum corrupti culpa odio. Repellat, rerum animi, explicabo culpa doloribus mollitia voluptatem at nam ab officia vitae.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
         </section> 
     )
 }
