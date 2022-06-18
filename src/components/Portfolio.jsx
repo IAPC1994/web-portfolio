9b26042053513a3b6426a273a2624b3b590f8a68
Ivan Panussis <ivan.panussis.c@gmail.com>
2022-06-18 04:51:42 -0400

    Swiper React in portfolio installed

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
 import { PortfolioItem } from './PortfolioItem';
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Pagination, Navigation } from "swiper";
 
 export const Portfolio = () => {
     return (
@@ -8,9 +16,30 @@ export const Portfolio = () => {
                 <h5>My personal work experience</h5>
                 <h1>Portfolio</h1>
                 <div className='portfolio__content'>
-                    <PortfolioItem title={"Project Name"} demo={"https://www.google.com"} github={"https://www.google.com"} image={"image.path"}/>
-                    <PortfolioItem title={"Project Name"} demo={"https://www.google.com"} github={"https://www.google.com"} image={"image.path"}/>
-                    <PortfolioItem title={"Project Name"} demo={"https://www.google.com"} github={"https://www.google.com"} image={"image.path"}/>
+                    <Swiper
+                         slidesPerView={3}
+                         spaceBetween={20}
+                         loop={false}
+                         pagination={{
+                           clickable: true,
+                         }}
+                         navigation={true}
+                         modules={[Pagination, Navigation]}
+                         className="mySwiper"
+                    >
+                        <SwiperSlide> 
+                            <PortfolioItem title={"Project Name 1"} demo={"https://www.google.com"} github={"https://www.google.com"} image={"image.path"}/>
+                        </SwiperSlide>
+                        <SwiperSlide> 
+                            <PortfolioItem title={"Project Name 2"} demo={"https://www.google.com"} github={"https://www.google.com"} image={"image.path"}/>
+                        </SwiperSlide>
+                        <SwiperSlide> 
+                            <PortfolioItem title={"Project Name 3"} demo={"https://www.google.com"} github={"https://www.google.com"} image={"image.path"}/>
+                        </SwiperSlide>
+                        <SwiperSlide> 
+                            <PortfolioItem title={"Project Name 4"} demo={"https://www.google.com"} github={"https://www.google.com"} image={"image.path"}/>
+                        </SwiperSlide>
+                    </Swiper>
                 </div>
             </div>
         </section>
--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const PortfolioItem = ({ title, image, github, demo }) => {
     return (
         <div className='portfolio__item'>
-
+            <h1>{ title }</h1>
         </div>
     )
 }
