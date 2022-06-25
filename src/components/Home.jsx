d9c1471b22655e44d51becd3c8bc0d770b9bee1a
Ivan Panussis <ivan.panussis.c@gmail.com>
2022-06-25 04:11:32 -0400

    Home section new style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,20 +4,28 @@ import Img from '../assets/perfil.jpg';
 export const Home = () => {
     return (
         <section id="home" className='home__section'>
-            <div className='home__container'>
-                <h3>Hello World!</h3>
-                <h1>I'm <a href="#about">Iván Panussis</a></h1>
-                <h5>Frontend Developer</h5>
-            </div>
 
-            <div className='home__img'>
-                <img src={ Img } alt="profile_photo"/>
-            </div>
+            <div className='home__content'>
+                <div className='home__container'>
+                    <div>
+                        <h1><a href="#about">Iván Alejandro Panussis Conejeros</a></h1>
+                        <br/>    
+                        <p>Hi, I am a professional Frontend and Backend developer. I have four years experience in IT Sector as a developer of web &amp; mobile applications.</p>
+                    </div>      
+                              
+                    <h5><a href='#contact'><i className="fa-solid fa-message"></i> Hire Me</a></h5>
+                </div>
 
-            <div className='home__button'>
+                <div className='home__img'>
+                    <img src={ Img } alt="profile_photo"/>
+                </div>
+            </div>
+           
+
+            {/* <div className='home__button'>
                 <button><i className="fa-solid fa-download"></i>  Download CV</button>
                 <button><i className="fa-solid fa-message"></i>  Contact me</button>
-            </div>
+            </div> */}
         </section>
     )
 }
